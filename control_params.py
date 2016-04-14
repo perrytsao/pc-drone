86cb5cea64c6d7f0414947f1b00a2539379a841f
Perry Tsao <perrytsao@gmail.com>
2016-04-13 20:19:37 -0700

    control parameters

---
+++ control_params.py
@@ -0,0 +1,33 @@
+Kpz=10.0
+Kiz=1
+Kdz=20
+Kz=-2
+
+Kpx=1
+Kix=.1
+Kdx=25
+Kx=-10
+
+Kpy=Kpx
+Kiy=Kix
+Kdy=Kdx
+
+#Kpy=50.0
+#Kiy=2
+#Kdy=3
+Ky=-1*Kx
+
+#############################
+# Position Gains
+Ksz=.1
+Ksx=.01
+Ksy=.01
+
+# Velocity Filter gains
+Fx=0.5
+Fy=0.5
+Fz=0.5
+#############################
+THROTTLE_MID=1350
+ELEVATOR_MID=1500
+AILERON_MID=1500
